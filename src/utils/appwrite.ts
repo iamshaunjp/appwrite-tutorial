35dc23c3a1112b53afb49609031d31fc03cf6efa
iamshaunjp <shaun@thenetninja.co.uk>
2024-09-16 17:13:44 +0100

    lesson-2

---
+++ src/utils/appwrite.ts
@@ -0,0 +1,7 @@
+import { Client, Databases } from 'appwrite'
+
+export const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+
+export const databases = new Databases(client)
