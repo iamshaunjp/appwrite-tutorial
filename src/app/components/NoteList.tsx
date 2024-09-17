934c2b8c2db0ad6a439d84c0a576195b270903b0
iamshaunjp <shaun@thenetninja.co.uk>
2024-09-17 13:57:28 +0100

    lesson-6

--- src/app/components/NoteList.tsx
+++ src/app/components/NoteList.tsx
@@ -1,9 +1,32 @@
 'use client'
-import { useState} from 'react'
+import { useEffect, useState} from 'react'
 import { deleteNote } from '../actions/noteActions'
+import { client } from '@/utils/appwrite'
 
 export default function NoteList({ initialNotes }: { initialNotes: Note[] }) {
   const [notes, setNotes] = useState<Note[]>(initialNotes)
+
+  useEffect(() => {
+    const channel = 'databases.notesApp.collections.notes.documents'
+
+    const unsubscribe = client.subscribe(channel, (response) => {
+      const eventType = response.events[0]
+      console.log(response.events)
+      const changedNote = response.payload as Note
+
+      if (eventType.includes('create')) {
+        setNotes((prevNotes) => [changedNote, ...prevNotes])
+      } 
+
+      if (eventType.includes('delete')) {
+        setNotes((prevNotes) =>
+          prevNotes.filter((note) => note.$id !== changedNote.$id)
+        )
+      }
+    })
+
+    return () => unsubscribe()
+  }, [])
 
   const handleDelete = async (noteId: string) => {
     const element = document.getElementById(noteId)
